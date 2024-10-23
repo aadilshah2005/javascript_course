fce0dece3db5ad7519d5f72f9061d23de8f17d32
aadilshah2005 <154650179+aadilshah2005@users.noreply.github.com>
2024-10-23 10:20:21 +0000

    container config done

---
+++ variable.js
@@ -0,0 +1,20 @@
+const accountId = 123442;
+let accountEmail = "Adil@gmail.com";
+var accountPassword = "Adil shah";
+accountcity  = "BHadohi";
+let accountState ;
+
+//accountId = 1211;
+accountEmail = "shan@gmail.com";
+accountPassword = "shan@121";
+accountcity = "Allhabad";
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountcity);
+
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState]);
+
+
+
